622f5a82b97effda14bf336f2f372301fe80964a
sudipsaha998 <sudipsaha998@gmail.com>
2019-09-01 03:53:56 +0530

    node deletion and preorder,postorder,inorder

--- src/sudip/tree/BinarSearchTree.java
+++ src/sudip/tree/BinarSearchTree.java
@@ -30,17 +30,81 @@ public class BinarSearchTree {
 			return minVal(root.left);
 	}
 	
+	private void delete(int val) {
+		root = deleteRec(root, val);
+	}
+	
+	private Node deleteRec(Node root, int val) {
+	
+		if(root == null) {
+			return root;
+		}
+		else if(val > root.key){
+			root.right = deleteRec(root.right, val);
+		}
+		else if(val < root.key) {
+			root.left = deleteRec(root.left, val);
+		}
+		else {
+			if(root.left == null) {
+				return root.right;
+			}
+			if(root.right == null) {
+				return root.left;
+			}
+			
+			root.key = minVal(root.right).key;
+			
+			root.right = deleteRec(root.right, root.key);
+		}
+		
+		return root;
+	}
+	
+	private void inorder(Node root) {
+		if(root != null) {
+			inorder(root.left);
+			System.out.print(root.key+" ");
+			inorder(root.right);
+		}
+		
+	}
+	
+	private void preorder(Node root) {
+		if(root != null) {
+			System.out.print(root.key+" ");
+			preorder(root.left);
+			preorder(root.right);
+		}
+		
+	}
+	
+	private void postorder(Node root) {
+		if(root != null) {
+			postorder(root.left);
+			postorder(root.right);
+			System.out.print(root.key+" ");
+		}
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		BinarSearchTree bst = new BinarSearchTree();
-		bst.insert(5);
-		bst.insert(3);
-		bst.insert(7);
-		bst.insert(4);
-		System.out.println(bst.minVal(bst.root).key);
+		bst.insert(50); 
+		bst.insert(30); 
+		bst.insert(20); 
+		bst.insert(40); 
+		bst.insert(70); 
+		bst.insert(60); 
+		bst.insert(80); 
 
+		bst.inorder(bst.root);
+		
+		System.out.println("\n");
+		bst.delete(50);
+		bst.inorder(bst.root);
 	}
 
 }
